132e553c7f7714d6ece79012eff76fca1d2062e9
James Warner <jamesnwarner@gmail.com>
2020-07-12 10:26:36 +0100

    feat(*): Implement initial code to fetch numbers from JSON file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BingoNumberService } from "./bingo-number-service";
+import { BingoNumber } from "./bingo-number";
+
+let bingoNumbers: BingoNumber[] = [];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'british-bingo-generator';
+export class AppComponent implements OnInit {
+  constructor(private bingoNumberService: BingoNumberService) {}
+
+  ngOnInit() {
+    console.log(bingoNumbers);
+
+    this.bingoNumberService.getJson().subscribe((data) => {
+      console.log("GOT NUMBERS!");
+      console.log(data);
+      bingoNumbers = data;
+
+      bingoNumbers.forEach((number) => {
+        console.log(`Number: ${number.value}`);
+      });
+    });
+  }
 }
